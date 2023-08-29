90ffe940e38bfadc187cb9a9d63ae3d7a3a22560
Abdul-2050 <90139459+Abdul-2050@users.noreply.github.com>
2023-08-29 13:39:21 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const config = {
+    apiKey: "AIzaSyC6QbAS32BJ8DS26rlWAKke8Mai63TujNg",
+    authDomain: "polify-42ea4.firebaseapp.com",
+    databaseURL: "https://polify-42ea4-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "polify-42ea4",
+    storageBucket: "polify-42ea4.appspot.com",
+    messagingSenderId: "92933707190",
+    appId: "1:92933707190:web:814cbac948fec6f9089d17"
+};
+
+export default config;
